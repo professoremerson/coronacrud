499e025e5ad46834d3ee0956bfbcfabb48ee4a85
professoremerson <emerson.schafhauser@professor.unc.br>
2021-08-16 22:17:09 -0300

    Criaçao do Controller, definiçao da rota, criaçao das blades

---
+++ app/Http/Controllers/CoronaController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoronaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // obtendo todos os dados do banco
+        $coronaCases = Corona::all();
+        // retornando a tela de visualização com os
+        // objetos obtidos do banco
+        return view('coronas.index', compact('coronaCases'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // exibindo a tela para cadastro
+        return view('coronas.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // criando um objeto para testar/aplicar 
+        // validações nos dados da requisição
+        $validatedData = $request->validate([
+            'country_name' => 'required|max:255',
+            'symptoms' => 'required',
+            'cases' => 'required|numeric',
+        ]);
+        // criando um objeto para receber o resultado
+        // da persistência dos dados validados 
+        $show = Corona::create($validatedData);
+        // redirecionando para o diretório raiz (index)
+        return redirect('/coronas')->with('success', 
+        'Dados de Corona adicionado com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // criando um objeto para receber o resultado
+        // da busca de registro/objeto específico
+        $coronaCase = Corona::findOrFail($id);
+        // retornando a tela de visualização com o
+        // objeto recuperado
+        return view('coronas.show',compact('coronaCase'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // criando um objeto para receber o resultado
+        // da busca de registro/objeto específico
+        $coronaCase = Corona::findOrFail($id);
+        // retornando a tela de edição com o
+        // objeto recuperado
+        return view('coronas.edit', compact('coronaCase'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // criando um objeto para testar/aplicar 
+        // validações nos dados da requisição
+        $validatedData = $request->validate([
+            'country_name' => 'required|max:255',
+            'symptoms' => 'required',
+            'cases' => 'required|numeric',
+        ]);
+        // criando um objeto para receber o resultado
+        // da persistência (atualização) dos dados validados 
+        Corona::whereId($id)->update($validatedData);
+        // redirecionando para o diretório raiz (index)
+        return redirect('/coronas')->with('success', 
+        'Dados de Corona atualizado com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // localizando o objeto que será excluído
+        $coronaCase = Corona::findOrFail($id);
+        // realizando a exclusão
+        $coronaCase->delete();
+        // redirecionando para o diretório raiz (index)
+        return redirect('/coronas')->with('success', 
+        'Dados de Corona removido com sucesso!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// criando uma para um grupo de recursos específico
+Route::resource('coronas','CoronaController');
