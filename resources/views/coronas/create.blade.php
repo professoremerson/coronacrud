f83814cddb2f906cff1a2ca7267858cd196ec1f2
professoremerson <emerson.schafhauser@professor.unc.br>
2021-08-23 19:36:46 -0300

    Criação das blades do CRUD

--- resources/views/coronas/create.blade.php
+++ resources/views/coronas/create.blade.php
@@ -1,7 +1,6 @@
-                    
 @extends('coronas.layout')
 
-@section('title',__('Criar (Corona CRUD Laravel) - i9W3b'))
+@section('title',__('Criar (Corona CRUD Laravel)'))
 
 @push('css')
 <style>
@@ -16,7 +15,7 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex justify-content-between w-100">
-                        <span>@lang('Criar (Corona CRUD Laravel) - i9W3b')</span>
+                        <span>@lang('Criar (Corona CRUD Laravel)')</span>
                         <a href="{{ url('coronas') }}" class="btn-info btn-sm">
                             <i class="fa fa-arrow-left"></i> @lang('Voltar')
                         </a>
